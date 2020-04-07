947521f814b8c0dbd2cf3c257e6dc724c8ead9ca
wako-p <o9o76gp381w@gmail.com>
2020-04-07 16:27:53 +0900

    format関数名を変更
    format→usd
    usd内で1/100に変換するように修正

--- chapter-1/statement.js
+++ chapter-1/statement.js
@@ -11,16 +11,16 @@ function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // 注文の内訳を出力
-        result += `${ playFor(perf).name }: ${ format(amountFor(perf) / 100) } (${ perf.audience } seats)\n`;
+        result += `${ playFor(perf).name }: ${ usd(amountFor(perf)) } (${ perf.audience } seats)\n`;
         totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${ usd(totalAmount) }\n`;
     result += `You earned ${ volumeCredits } credits\n`;
 
     return result;
 
-    function format() {
+    function usd(aNumber) {
         return new Intl.NumberFormat(
             "en-US",
             {
@@ -28,7 +28,7 @@ function statement(invoice, plays) {
                 currency:              "USD",
                 minimumFractionDigits: 2
             }
-        ).format;
+        ).format(aNumber/100);
     }
 
     function volumeCreditsFor(aPerformance) {
