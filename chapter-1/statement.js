94267bb9fae032b7d009ae78d9a9b76ded76a19c
wako-p <o9o76gp381w@gmail.com>
2020-04-08 16:15:20 +0900

    データオブジェクトに演劇料金情報を追加

--- chapter-1/statement.js
+++ chapter-1/statement.js
@@ -12,13 +12,42 @@ function statement(invoice, plays) {
 
         const result = Object.assign({}, aPerformance);
 
-        result.play = playFor(result);
+        result.play   = playFor(result);
+        result.amount = amountFor(result);
 
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+
+        let result = 0;
+
+        switch (aPerformance.play.type)
+        {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+    
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + (500 * (aPerformance.audience - 20));
+                }
+                result += 300 * aPerformance.audience;
+                break;
+    
+            default:
+                throw new Error(`unknown type: ${ aPerformance.play.type }`);
+        }
+
+        return result;
     }
 }
 
@@ -27,7 +56,7 @@ function renderPlaneText(data, plays) {
     let result = `Statement for ${ data.customer }\n`;
 
     for (let performance of data.performances) {
-        result += `${ data.play.name }: ${ usd(amountFor(performance)) } (${ performance.audience } seats)\n`;
+        result += `${ performance.play.name }: ${ usd(performance.amount) } (${ performance.audience } seats)\n`;
     }
 
     result += `Amount owed is ${ usd(totalAmount()) }\n`;
@@ -40,7 +69,7 @@ function renderPlaneText(data, plays) {
         let result = 0;
 
         for (let performance of data.performances) {
-            result += amountFor(performance);
+            result += performance.amount;
         }
 
         return result;
@@ -76,34 +105,6 @@ function renderPlaneText(data, plays) {
         // 喜劇の場合は10人につき、さらにポイント加算
         if (aPerformance.play.type === "comedy") {
             result += Math.floor(aPerformance.audience / 5);
-        }
-
-        return result;
-    }
-
-    function amountFor(aPerformance) {
-
-        let result = 0;
-
-        switch (aPerformance.play.type)
-        {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-    
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + (500 * (aPerformance.audience - 20));
-                }
-                result += 300 * aPerformance.audience;
-                break;
-    
-            default:
-                throw new Error(`unknown type: ${ aPerformance.play.type }`);
         }
 
         return result;
