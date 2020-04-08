ed79371fc11e9a0f1272fbd117762b1df7ae065a
wako-p <o9o76gp381w@gmail.com>
2020-04-08 16:06:03 +0900

    データオブジェクトに公演情報をコピーするように修正

--- chapter-1/statement.js
+++ chapter-1/statement.js
@@ -4,9 +4,16 @@ function statement(invoice, plays) {
     const statementData = {};
 
     statementData.customer     = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance(performance));
 
     return renderPlaneText(statementData, invoice, plays);
+
+    function enrichPerformance(aPerformance) {
+
+        const result = Object.assign({}, aPerformance);
+
+        return result;
+    }
 }
 
 function renderPlaneText(data, plays) {
