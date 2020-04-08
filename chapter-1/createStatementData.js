384cd7e3afbeff761d5f4622cbf6c485e0ea0cd6
wako-p <o9o76gp381w@gmail.com>
2020-04-08 17:16:10 +0900

    PerformanceCalculatorクラスを作成

--- chapter-1/createStatementData.js
+++ chapter-1/createStatementData.js
@@ -1,4 +1,11 @@
 
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play        = aPlay;
+    }
+}
+
 export default function createStatementData(invoice, plays) {
 
     const statementData = {};
@@ -12,9 +19,11 @@ export default function createStatementData(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
 
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+
         const result = Object.assign({}, aPerformance);
 
-        result.play          = playFor(result);
+        result.play          = calculator.play;
         result.amount        = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
 
